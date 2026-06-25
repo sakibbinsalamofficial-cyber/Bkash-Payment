6af04cb4ef69db24425905c196fd0f8f7b35bbb8
sakibbinsalamofficial-cyber <sakibbinsalamofficial@gmail.com>
2026-06-25 15:22:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function copyNumber(){
-    navigator.clipboard.writeText("01410509247");
+
+    const number =
+    document.getElementById("number").innerText;
+
+    navigator.clipboard.writeText(number);
+
     alert("Number copied");
+}
+
+function verifyTransaction(){
+
+    const trx =
+    document.getElementById("trx").value.trim();
+
+    if(trx === ""){
+        alert("Please enter Transaction ID");
+        return;
+    }
+
+    alert("Transaction Submitted: " + trx);
 }
