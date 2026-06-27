49be3fc4e1a5628d1ac6a4678b787bc2eea75793
Sakib Bin Salam <sakibbinsalamofficial@gmail.com>
2026-06-27 18:31:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ navigator.clipboard.writeText(number)
         alert("Number copied successfully!");
     })
     .catch(() => {
-        alert("Copy Successfully");
+        alert("Copy Failed!");
     });
 ```
 
